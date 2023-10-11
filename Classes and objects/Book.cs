2e329288ab577c992094a4ac552ba00a62a315bc
Lenovo-User <Lenovo-User@DESKTOP-RNPFAS5>
2023-10-11 09:19:03 +0200

    Add project files.

---
+++ Classes and objects/Book.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes_and_objects
+{
+    internal class Book
+    {
+        public string tittle;
+        public string author;
+        public int pages;
+    }
+}
---
+++ Classes and objects/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes_and_objects
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Book book1 = new Book();
+
+            book1.tittle = "Harry Potter";
+            book1.author = "JH Rowling";
+            book1.pages = 400;
+
+            Book book2 = new Book();
+
+            book2.tittle = "Lord of the rings";
+            book2.author = "Tolkein";
+            book2.pages = 900;
+
+            //book1
+            Console.WriteLine(book1.pages);
+            Console.WriteLine(book1.tittle);
+            Console.WriteLine(book1.author);
+
+            //book2
+            Console.WriteLine(book2.pages);
+            Console.WriteLine(book2.tittle);
+            Console.WriteLine(book2.author);
+
+            //freeze
+            Console.ReadLine();
+
+
+        }
+    }
+}
